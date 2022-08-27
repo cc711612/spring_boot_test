b998f5a90a9ade3af3a9df194265c7f7391d4ec4
Roy <cc711612@gmail.com>
2022-08-27 15:08:45 +0800

    Initial commit

---
+++ src/test/java/com/roy/spring_boot_mall/SpringBootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.spring_boot_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
